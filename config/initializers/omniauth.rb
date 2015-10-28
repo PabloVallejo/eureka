ddac52932035a17e82af62226d9cbb9402aa39ae
Pablo Vallejo <pabloninety@gmail.com>
2015-10-27 23:00:15 -0500

    Cleanup: Remove unused files

--- config/initializers/omniauth.rb
+++
@@ -1,8 +0,0 @@
-#
-# Facebook Omniauth strategy
-#
-Rails.application.config.middleware.use OmniAuth::Builder do
-  # provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'],
-  provider :facebook, '908051069244382', '4ad7daf43c53ac1d649867eebddcd5fe',
-           :scope => 'email', :display => 'popup'
-end
